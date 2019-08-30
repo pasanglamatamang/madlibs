a19d5924d1b5cc302cd91e1144d2733a02a9052a
pasanglamatamang <pasang.lama.tamang11@gmail.com>
2019-08-29 22:55:52 -0700

    A simple program design

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,15 @@
 """  Story Format:
 i was ____(verb -ing) in the top of the mountain. I ____ (verb) for really long time. The ____(noun) was gigantic and _____(adjective). i could ____(verb) the sky, _____(verb) the ground, and ____(verb) the sun. The ____(plural noun) were chirping. The____(noun) was howling. """
+
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+verb2 = input("Enter a verb: ")
+verb_ing = input("Enter a verb -ing: ")
+verb3 = input("Enter a verb: ")
+pluralnoun = input("Enter a pluralnoun: ")
+verb4 = input("Enter a verb: ")
+noun2 = input("Enter a noun: ")
+
+print("I was " + verb_ing + " in the top of the mountain. I " + verb1 + "for really long      time. The " + noun1 + " was gigantic and " + adjective +
+      ". I could " + verb2 + " the sky, " + verb3 + " the ground, and " + verb4 + " the sun. The " + pluralnoun + " were chirping. The " + noun2 + " was howling.")

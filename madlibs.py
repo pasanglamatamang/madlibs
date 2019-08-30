421e8e6d6d7fb10ff8a1c997cb5617f05e604c56
pasanglamatamang <pasang.lama.tamang11@gmail.com>
2019-08-29 20:56:56 -0700

    wrote the format of the story

---
+++ madlibs.py
@@ -0,0 +1,2 @@
+"""  Story Format:
+i was ____(verb -ing) in the top of the mountain. I ____ (verb) for really long time. The ____(noun) was gigantic and _____(adjective). i could ____(verb) the sky, _____(verb) the ground, and ____(verb) the sun. The ____(plural noun) were chirping. The____(noun) was howling. """
